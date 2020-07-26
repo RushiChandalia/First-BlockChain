5b2195eefda85916d7a04f73e925265d80016b95
Rushi Chandalia <rushichandalia.ict17@gmail.com>
2020-07-26 12:46:30 +0530

    block chain created mining is left

--- blockchain.py
+++ blockchain.py
@@ -2,6 +2,8 @@ import datetime
 import hashlib
 import json
 
+
+
 # initializing blockchain
 
 
@@ -17,7 +19,7 @@ class Blockchain:
             "index": len(self.chain + 1),
             "timestamp": str(datetime.datetime.now()),
             "proof": proof,
-            "previoushash": previous_hash
+            "previous_hash": previous_hash
         }
         self.chain.append(block)
         return block
@@ -25,4 +27,31 @@ class Blockchain:
     def get_previous_block(self):  # getting last block for previous hash
         return self.chain[-1]
 
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2-previous_proof**2).encode).hexdigest
+            if(hash_operation[:4]=='0000'):
+                check_proof = True
+            else:
+                new_proof+=1
+        return new_proof
+
+    def hash_block(self,block):
+        encoded_block = json.dumps(block, sort_keys=True).encode
+        return hashlib.sha256(encoded_block).hexdigest
+
+    def block_is_valid(self,chain):
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if(block['previous_hash'] != self.hash_block(previous_block)):
+                return False
+            
+
+
+    
 # mining the block
+
